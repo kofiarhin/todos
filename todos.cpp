b10d2e0afa2081c35455767f0341c2bccc2d0a5d
kofi arhin <kofiarhin69@gmail.com>
2026-07-26 04:59:15 +0100

    init

---
+++ todos.cpp
@@ -0,0 +1,14 @@
+//todo 
+
+using the current zoro heroku less jhumban in the room strategy as an inspiration. will do the same for vercel and github
+
+2. next actionalble step by claude code
+
+3. run a full test of the current state of zoro -- full end to end manual testing
+4. ask architech what other capabliities can i add and how should i structure or optimize the capabliities and make it scalableto add things like skills or mcp or other adapters
+
+4. test out zoro on a real project -> feature implementation 
+
+5. setup a dtodos repository and give access to zoro this will be my manual request or brainstorming point where i can ask zoro to do random stuff. like priveate inbox or ont he go stuff.
+
+6. add email capabilitity to zoro -> read and reply email. notification and email summary 
